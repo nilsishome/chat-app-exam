a1d982a292a03426fc39769f5ffac74cb3911aff
nilsishome <nilsnyberg24@gmail.com>
2026-01-15 12:01:38 +0100

    Small fixes for types

--- backend/db.ts
+++ backend/db.ts
@@ -9,7 +9,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
+  port: Number(process.env.POSTGRES_PORT),
   database: process.env.POSTGRES_NAME,
 });
 
