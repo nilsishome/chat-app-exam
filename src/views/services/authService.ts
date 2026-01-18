6ce944787cc0235c9a8d99253118ebfe4fb65418
nilsishome <nilsnyberg24@gmail.com>
2026-01-18 18:27:25 +0100

    Relocate types.ts file + add standalone types.ts file to backend

--- src/views/services/authService.ts
+++ src/views/services/authService.ts
@@ -1,5 +1,5 @@
 import type { Ref } from "vue";
-import type { credentials } from "../types";
+import type { credentials } from "@/types";
 
 export const signUpData = async (credentials: credentials) => {
   try {
