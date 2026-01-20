76c5a21a27310944c2e153bd3516ebb54a245f80
nilsishome <nilsnyberg24@gmail.com>
2026-01-20 15:35:37 +0100

    Add message bar + start styling

--- src/chat/components/Chat.vue
+++ src/chat/components/Chat.vue
@@ -29,6 +29,16 @@ watch(
       :id="messageData.sender"
       :msg="messageData"
     />
+    <div>
+      <input
+        id="messageTextBar"
+        type="text"
+        minlength="1"
+        placeholder="Skriv ett meddelande..."
+        required
+      />
+      <i class="pi pi-search sendIcon" aria-hidden="true"></i>
+    </div>
   </div>
   <div v-else class="errorMessage">
     <h2>Du är för närvarande inte i en chatt. Välj en chatt för att se meddelanden.</h2>
@@ -57,5 +67,39 @@ h2 {
 .chatWrapper {
   display: flex;
   flex-direction: column;
+}
+
+#messageTextBar {
+  position: fixed;
+  bottom: 2.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  padding-left: 1rem;
+  width: 45rem;
+  height: 2rem;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+}
+
+.sendIcon {
+  left: 0.5rem;
+  top: 50%;
+  transform: translateY(-50%);
+  color: black;
+  pointer-events: none;
+  font-size: 0.8rem;
+}
+
+@media (min-width: 1024px) {
+  #messageTextBar {
+    transform: translateX(calc(-50% + 15rem));
+  }
+}
+
+@media (min-width: 2100px) {
+  #messageTextBar {
+    transform: translateX(calc(-50% + 21rem));
+  }
 }
 </style>
