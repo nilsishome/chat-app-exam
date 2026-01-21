b7ed8af4f0f1d4edd508db061b5e750b3ae83d07
nilsishome <nilsnyberg24@gmail.com>
2026-01-21 13:15:47 +0100

    Make user sections in frontend use user data from DB

--- src/chat/store/user.ts
+++ src/chat/store/user.ts
@@ -3,11 +3,20 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore("user", {
   state: () => {
     return {
-      name: "Fredrik Svensson" as string,
+      name: "" as string,
       status: true as boolean,
-      id: 1 as number,
+      id: 0 as number,
       image:
         "https://img.freepik.com/free-photo/portrait-sensitive-man_23-2149444531.jpg?semt=ais_incoming&w=740&q=80" as string,
     };
+  },
+
+  persist: true,
+
+  actions: {
+    setUser(name: string, id: number) {
+      this.name = name;
+      this.id = id;
+    },
   },
 });
